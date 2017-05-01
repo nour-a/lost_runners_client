a2a491ec5409b2424afaa87a68942e367315a649
Daryl Carr <darylcarr@live.co.uk>
2017-05-01 19:06:49 +0100

    [FEAT#11] Add Auth0 lock and modify app build.gradle files to exclude files from build process

--- android/app/src/main/java/com/lost_runner_client/MainApplication.java
+++ android/app/src/main/java/com/lost_runner_client/MainApplication.java
@@ -3,6 +3,7 @@ package com.lost_runner_client;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.auth0.lock.react.LockReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LockReactPackage(),
           new VectorIconsPackage(),
           new MapsPackage(),
           new ReactNativeContacts()
